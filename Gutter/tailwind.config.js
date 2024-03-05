50f6e4135d4774882061324393e6cf72e12b3408
KaramDargham <karamdargham1@gmail.com>
2024-03-06 01:23:41 +0200

    test

---
+++ Gutter/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primaryColor:'#01314A',
+        secondaryColor:'#e85f3c'
+      }
+    },
+  },
+  plugins: [],
+}
+
