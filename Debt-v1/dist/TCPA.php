acb63d777861afcabfc4da2cdf9a278a2f59fbed
KaramDargham <karamdargham1@gmail.com>
2024-03-22 20:19:48 +0200

    add new pages

---
+++ Debt-v1/dist/TCPA.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TCPA</title>
+    <link href="/Debt-v1/dist/output.css" rel="stylesheet">
+</head>
+<body>
+<nav class="bg-white shadow-lg">
+    <div class="block">
+        <div class="flex justify-center">
+            <div class="py-2 brand flex justify-center">
+                <img src="/Debt-v1/dist/img/logo.png">
+            </div>
+        </div>
+    </div>
+</nav>
+<h1 class="bg-primary text-white text-3xl lg:py-8">TCPA Disclosure</h1>
+<div class="px-28 pt-5">
+
+
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>By clicking &quot;Verify My Phone Number,&quot; you hereby agree to the following terms in accordance with the Telephone Consumer Protection Act (TCPA):</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;vertical-align:baseline;'><span style='font-size:12px;font-family:"Segoe UI",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>You consent to be contacted and receive informational and marketing communications from us and/or our service providers, affiliates, third parties and/ or <u><span style="color:#00B0F0;">Partners,</span></u> and our related affiliates, subsidiaries, or assigns (collectively &ldquo;Partners&rdquo;) These communications may be delivered by live agents, artificial, automated, or pre-recorded voice, via email, and Short Message Service (&quot;SMS&quot;) text to your residence, mobile, or other provided phone numbers or addresses you have provided, or that you will provide in the future (of which you hereby represent you are the owner, subscriber, or regular user) (collectively, &quot;<strong>Contact Info</strong>&quot;). Communications may be dialed manually or by an automated telephone dialing system. You acknowledge that standard message and data rates may apply for SMS messages.</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:12px;font-family:"Segoe UI",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>Your consent extends to being contacted even if your contact info is on ours or our Partners&apos; Do Not Call/Do Not Email List, a State or National Do Not Call Registry, or any other Do Not Contact List. You consent to calls made to the phone number associated with the webform through which this consent is given. You commit to promptly notify us if you cease using a specific phone number or email address. Consent for contact is not a prerequisite for purchasing services, and you retain the right to revoke this consent at any time by emailing us at&nbsp;</span><a href="mailto:compliance@jugglingdebt.com"><span style='font-size:15px;font-family:"Calibri",sans-serif;'>compliance@jugglingdebt.com</span></a><span style='font-size:15px;font-family:"Calibri",sans-serif;'>.</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:12px;font-family:"Segoe UI",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>You consent to and acknowledge the possibility of automated messages or artificial/pre-recorded voices being played when the telephone is answered whether by you or another party. Additionally, you acknowledge that we and/or our <u><span style="color:#00B0F0;">Partners</span></u> may listen, record, and retain recordings of calls for quality assurance or other purposes, as permitted by applicable laws, without further notice.</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:12px;font-family:"Segoe UI",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>You agree and acknowledge that we may disclose, share, sell, re-sell, or market your personal data to one or more Partner(s), which may result in compensation to us or our Partners. You consent to the use of this data for purposes, including re-targeting, marketing, analytics, product, service improvement and to enhance user experience. Such disclosures may involve sharing and/or selling your data to third-party service providers, brokers, and their service providers or Partners. You expressly consent to being contacted for marketing purposes regarding debt relief or debt consolidation services that our Company offers directly or through a third party, that we may deem a potential fit for you, based on the information you have submitted to us or our <u><span style="color:#00B0F0;">Partners</span></u>.</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:12px;font-family:"Segoe UI",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>You consent to the use of analytic and marketing tools, including Artificial Intelligence-based software and technology, utilizing your data. This includes the use of technology to track your consent, capture screenshots, and record your IP address or other digital footprint evidence to validate your consent.</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:12px;font-family:"Segoe UI",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>Our services may include SMS messages, with a limit of up to five messages per day to your wireless device. We may use your information to contact you about your info, purchases, website updates, conduct surveys, or informational and service-related communications, including important security updates. You can opt-out at any time by replying &quot;STOP&quot; to received SMS messages. Upon sending &quot;STOP,&quot; we will confirm your unsubscribing via SMS, after which you will no longer receive messages. To rejoin, simply sign up again. For help, please reply to a text with HELP. Message and data rates may apply, contingent on your cell phone plan. Carriers are not responsible for delayed or undelivered messages. Alternatively, submit your request to stop SMS messages by emailing us at&nbsp;</span><a href="mailto:unsubscribe@jugglingdebt.com"><span style='font-size:15px;font-family:"Calibri",sans-serif;'>unsubscribe@jugglingdebt.com</span></a><span style='font-size:15px;font-family:"Calibri",sans-serif;'>, including your registered email address and phone number.</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;text-align:justify;vertical-align:baseline;'><span style='font-size:15px;font-family:"Calibri",sans-serif;'>You understand that you are not obligated to consent to receive calls or text messages as a condition for purchasing goods or services. You may continue your application by sending an email to&nbsp;</span><a href="mailto:info@jugglingdebt.com"><span style='font-size:15px;font-family:"Calibri",sans-serif;'>info@jugglingdebt.com</span></a><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;. You also agree to Juggling Debts&rsquo; <u>Privacy Policy&nbsp;</u>and <u>Terms of Use</u>, opting to receive important notices and other communications electronically. Furthermore, you consent to us and/or our Partners performing a credit check based on the information you provided.</span><span style='font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</span></p>
+    <p style='margin-top:0cm;margin-right:0cm;margin-bottom:8.0pt;margin-left:0cm;font-size:11.0pt;font-family:"Calibri",sans-serif;'>&nbsp;</p>
+</div>
+<footer class="bg-primary pt-5 pb-20 mt-10">
+    <div class="grid lg:grid-cols-6 text-white">
+        <div class="flex justify-center">
+            <a href="">Unsubscribe</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="">Do Not Sell My Information</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/privacy_policy.php">Privacy Policy</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/Terms.php">Terms of Use</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/partners.php">Partners</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/TCPA.php">TCPA Disclosure</a>
+        </div>
+    </div>
+    <div class="pt-8 text-center text-white px-5 lg:px-24" style="font-size: 13px">
+        IMPORTANT ADVERTISER DISCLAIMERS:  Programs do not guarantee that your debts will be lowered by a specific amount or percentage or that you will be debt-free within a specific period of time. Programs do not assume consumer debt, make monthly payments to creditors or provide tax, bankruptcy, accounting or legal advice or credit repair services. Programs are not available in all states, and fees may vary by state. Please contact a tax professional to discuss tax consequences of settlement. Please consult with a bankruptcy attorney for more information on bankruptcy. Depending on your state, programs may be available to recommend a local tax professional and/or bankruptcy attorney. You may be subject to collections or lawsuits by creditors or collectors. Your outstanding debt may increase from the accrual of fees and interest. Read and understand all program materials prior to enrollment, including potential adverse impact on credit rating.
+
+        Certain types of debts are not eligible for enrollment. Some creditors are not eligible for enrollment because they do not negotiate with debt relief companies.
+
+        The company and its affiliates are not lenders, creditors, or debt collectors. This is not a loan. Our representatives have helped thousands of consumers throughout their careers.
+    </div>
+</footer>
+</body>
+</html>
---
+++ Debt-v1/dist/partners.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partners</title>
+    <link href="/Debt-v1/dist/output.css" rel="stylesheet">
+</head>
+<body>
+<nav class="bg-white shadow-lg">
+    <div class="block">
+        <div class="flex justify-center">
+            <div class="py-2 brand flex justify-center">
+                <img src="/Debt-v1/dist/img/logo.png">
+            </div>
+        </div>
+    </div>
+</nav>
+<h1 class="bg-primary text-white text-3xl lg:py-8">Partners</h1>
+<h3 class="text-3xl text-center" style="padding-top: 80px;padding-bottom: 40px">List of Partners:</h3>
+   <p class="text-3xl  text-center" style="padding-bottom: 180px"> Symple Lending </p>
+<footer class="bg-primary pt-5 pb-20 mt-10">
+    <div class="grid lg:grid-cols-6 text-white">
+        <div class="flex justify-center">
+            <a href="">Unsubscribe</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="">Do Not Sell My Information</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/privacy_policy.php">Privacy Policy</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/Terms.php">Terms of Use</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/partners.php">Partners</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/TCPA.php">TCPA Disclosure</a>
+        </div>
+    </div>
+    <div class="pt-8 text-center text-white px-5 lg:px-24" style="font-size: 13px">
+        IMPORTANT ADVERTISER DISCLAIMERS:  Programs do not guarantee that your debts will be lowered by a specific amount or percentage or that you will be debt-free within a specific period of time. Programs do not assume consumer debt, make monthly payments to creditors or provide tax, bankruptcy, accounting or legal advice or credit repair services. Programs are not available in all states, and fees may vary by state. Please contact a tax professional to discuss tax consequences of settlement. Please consult with a bankruptcy attorney for more information on bankruptcy. Depending on your state, programs may be available to recommend a local tax professional and/or bankruptcy attorney. You may be subject to collections or lawsuits by creditors or collectors. Your outstanding debt may increase from the accrual of fees and interest. Read and understand all program materials prior to enrollment, including potential adverse impact on credit rating.
+
+        Certain types of debts are not eligible for enrollment. Some creditors are not eligible for enrollment because they do not negotiate with debt relief companies.
+
+        The company and its affiliates are not lenders, creditors, or debt collectors. This is not a loan. Our representatives have helped thousands of consumers throughout their careers.
+    </div>
+</footer>
+</body>
+</html>
