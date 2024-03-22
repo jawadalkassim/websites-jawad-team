341e10ece63b2ce37b6e042eda4c4c68c9046be7
KaramDargham <karamdargham1@gmail.com>
2024-03-22 20:18:45 +0200

    add new pages

--- Debt-v1/dist/index.php
+++ Debt-v1/dist/index.php
@@ -164,8 +164,15 @@
                             <div class="pt-1">
                                 <input type="checkbox" id="checkbox2" class="w-3" required />
                             </div>
-                            <span class="pl-2"><input type="hidden" id="leadid_tcpa_disclosure"/>By checking this box I consent to receive text messages and calls via automatic telephone dialing system and an artificial or prerecorded voice to the phone number I entered above from JugglingDebt.com as well as from debt relief solution companies, loan providers, debt consolidation service providers and partners, for debt relief or debt consolidation service offers even if I am on a national, state, or entity-specific Do-Not-Call list. I understand that consent is not required to receive a quotation or make a purchase. Message and data rates may apply. For more information, please review our TCPA Disclosure. </span>
-
+                            <span class="pl-2"><input type="hidden" id="leadid_tcpa_disclosure"/>By checking this box I consent to receive text messages and calls via
+automatic telephone dialing system and an artificial or prerecorded voice to
+the phone number I entered above from JugglingDebt.com as well as from debt
+relief solution companies, loan providers, debt consolidation service providers
+and Partners, for debt relief or debt consolidation service offers even if I am
+on a national, state, or entity-specific Do-Not-Call list. I understand that consent
+is not required to receive a quotation or make a purchase.  Message and
+data rates may apply. For more information, please review our <span class="font-bold text-primary"><a href="/Debt-v1/dist/TCPA.php">TCPA Disclosure. </a></span>
+                            </span>
                         </div>
                     </div>
                     <div class="flex justify-center py-10 items-center ">
@@ -190,7 +197,7 @@
     </form>
 </div>
 <footer class="bg-primary pt-5 pb-20 mt-10">
-    <div class="grid lg:grid-cols-4 text-white">
+    <div class="grid lg:grid-cols-6 text-white">
         <div class="flex justify-center">
             <a href="">Unsubscribe</a>
         </div>
@@ -203,18 +210,19 @@
         <div class="flex justify-center">
             <a href="/Debt-v1/dist/Terms.php">Terms of Use</a>
         </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/partners.php">Partners</a>
+        </div>
+        <div class="flex justify-center">
+            <a href="/Debt-v1/dist/TCPA.php">TCPA Disclosure</a>
+        </div>
     </div>
     <div class="pt-8 text-center text-white px-5 lg:px-24" style="font-size: 13px">
-        TCPA Disclosure
-        By clicking "Complete My Application," you hereby agree to the following terms in accordance with the Telephone Consumer Protection Act (TCPA):
-        You consent to be contacted and receive informational and marketing communications from us and/or our service providers, affiliates, third parties and/ or partners, (collectively “Partners”) These communications may be delivered by live agents, artificial, automated, or pre-recorded voice, via email, and Short Message Service ("SMS") text to your residence, mobile, or other provided phone numbers or addresses you have provided, or that you will provide in the future (of which you hereby represent you are the owner, subscriber, or regular user) (collectively, "Contact Info"). Communications may be dialed manually or by an automated telephone dialing system. You acknowledge that standard message and data rates may apply for SMS messages.
-        Your consent extends to being contacted even if your contact info is on ours or our Partners' Do Not Call/Do Not Email List, a State or National Do Not Call Registry, or any other Do Not Contact List. You consent to calls made to the phone number associated with the webform through which this consent is given. You commit to promptly notify us if you cease using a specific phone number or email address. Consent for contact is not a prerequisite for purchasing services, and you retain the right to revoke this consent at any time by emailing us at span class="font-bold"><span><a href="mailto:compliance@jugglingdebt.com">compliance@jugglingdebt.com,.</a> </span>
-        You consent to and acknowledge the possibility of automated messages or artificial/pre-recorded voices being played when the telephone is answered whether by you or another party. Additionally, you acknowledge that we and/or our Partners may listen, record, and retain recordings of calls for quality assurance or other purposes, as permitted by applicable laws, without further notice.
-        You agree and acknowledge that we may disclose, share, sell, re-sell, or market your personal data to one or more Partner(s), which may result in compensation to us or our Partners. You consent to the use of this data for purposes, including re-targeting, marketing, analytics, product, service improvement and to enhance user experience. Such disclosures may involve sharing and/or selling your data to third-party service providers, brokers, and their service providers or Partners. You expressly consent to being contacted for marketing purposes regarding debt relief or debt consolidation services that our Company offers directly or through a third party, that we may deem a potential fit for you, based on the information you have submitted to us or our Partners.
-        You consent to the use of analytic and marketing tools, including Artificial Intelligence-based software and technology, utilizing your data. This includes the use of technology to track your consent, capture screenshots, and record your IP address or other digital footprint evidence to validate your consent.
-        Our services may include SMS messages, with a limit of up to five messages per day to your wireless device. We may use your information to contact you about your info, purchases, website updates, conduct surveys, or informational and service-related communications, including important security updates. You can opt-out at any time by replying "STOP" to received SMS messages. Upon sending "STOP," we will confirm your unsubscribing via SMS, after which you will no longer receive messages. To rejoin, simply sign up again. For help, please reply to a text with HELP. Message and data rates may apply, contingent on your cell phone plan. Carriers are not responsible for delayed or undelivered messages. Alternatively, submit your request to stop SMS messages by emailing us at <span class="font-bold"><a href="mailto:compliance@jugglingdebt.com">compliance@jugglingdebt.com,</a> </span> including your registered email address and phone number.
-        You understand that you are not obligated to consent to receive calls or text messages as a condition for purchasing goods or services. You may continue your application by sending an email to <span class="font-bold"><a href="mailto:info@jugglingdebt.com">info@jugglingdebt.com </a> </span>. You also agree to Juggling Debts’ Privacy Policy and Terms of Use, opting to receive important notices and other communications electronically. Furthermore, you consent to us and/or our Partners performing a credit check based on the information you provided.
+        IMPORTANT ADVERTISER DISCLAIMERS:  Programs do not guarantee that your debts will be lowered by a specific amount or percentage or that you will be debt-free within a specific period of time. Programs do not assume consumer debt, make monthly payments to creditors or provide tax, bankruptcy, accounting or legal advice or credit repair services. Programs are not available in all states, and fees may vary by state. Please contact a tax professional to discuss tax consequences of settlement. Please consult with a bankruptcy attorney for more information on bankruptcy. Depending on your state, programs may be available to recommend a local tax professional and/or bankruptcy attorney. You may be subject to collections or lawsuits by creditors or collectors. Your outstanding debt may increase from the accrual of fees and interest. Read and understand all program materials prior to enrollment, including potential adverse impact on credit rating.
 
+        Certain types of debts are not eligible for enrollment. Some creditors are not eligible for enrollment because they do not negotiate with debt relief companies.
+
+        The company and its affiliates are not lenders, creditors, or debt collectors. This is not a loan. Our representatives have helped thousands of consumers throughout their careers.
     </div>
 </footer>
 
