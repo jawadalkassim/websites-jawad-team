7f6b2187d201157757d9779ebb36fa22b38d1337
Amjadvc <amjadalmrai@gmail.com>
2024-04-23 18:25:35 +0300

    copy solar and change a colos

---
+++ Solar_2/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#01314A",
+        secondaryColor: "#0d8155",
+      },
+    },
+  },
+  plugins: [],
+};
