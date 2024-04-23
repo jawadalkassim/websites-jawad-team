ce1d3a7636e672282efd30bdc3f007e442684faf
Amjadvc <amjadalmrai@gmail.com>
2024-04-23 20:04:20 +0300

    modifid solder-2 agin

--- Solar_2/dist/submit-page.php
+++ Solar_2/dist/submit-page.php
@@ -1,36 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Submit</title>
-    <link rel="stylesheet" href="/Solar/dist/output.css">
-</head>
-<body>
+    <link rel="stylesheet" href="/Solar_2/dist/output.css" />
+  </head>
 
-<nav class="text-center " style="background: white">
-    <div class="grid lg:grid-cols-2 md:grid-cols-1 items-center ">
-        <div class="flex justify-center items-center p-2 ">
-            <img src="/Solar/dist/img/logo.png" class="w-1/12 brand">
+  <body>
+    <nav class="text-center" style="background: white">
+      <div class="grid lg:grid-cols-2 md:grid-cols-1 items-center">
+        <div class="flex justify-center items-center p-2">
+          <img src="/Solar_2/dist/img/logo.png" class="w-1/12 brand" />
         </div>
-        <a href="" class="nav-link pt-3 text-primaryColor decoration-primaryColor font-bold text-xl block lg:text-end sm:text-center md:text-center lg:mx-40 md:mx-0 sm:mx-0 pb-3 lg:w-1/2 sm:w-full">Get Free Quotes</a>
+        <a
+          href=""
+          class="nav-link pt-3 text-primaryColor decoration-primaryColor font-bold text-xl block lg:text-end sm:text-center md:text-center lg:mx-40 md:mx-0 sm:mx-0 pb-3 lg:w-1/2 sm:w-full"
+          >Get Free Quotes</a
+        >
+      </div>
+    </nav>
+    <div
+      class="w-full grid grid-cols-3 p-3 justify-center shadow list-bar"
+      style="background: #6ab06d"
+    >
+      <div
+        class="text-center text-white font-bold flex justify-center items-center"
+      >
+        <span class="pr-2 icon w-8 flex justify-center items-center"
+          ><img src="/Solar_2/dist/img/chat.png" style="display: inline"
+        /></span>
+        <h1 class="flex justify-center items-center">
+          Get the best quotes, fast!
+        </h1>
+      </div>
+      <div
+        class="text-center text-white font-bold flex justify-center items-center"
+      >
+        <span class="pr-2 icon w-8 flex justify-center items-center"
+          ><img src="/Solar_2/dist/img/location.png" style="display: inline" />
+        </span>
+        <h1 class="flex justify-center items-center">
+          Over 5,000 engineers across the UK
+        </h1>
+      </div>
+      <div
+        class="text-center text-white font-bold flex justify-center items-center"
+      >
+        <span class="pr-2 icon w-8 flex justify-center items-center"
+          ><img src="/Solar_2/dist/img/check.png" style="display: inline"
+        /></span>
+        <h1 class="flex justify-center items-center">
+          Gas Safe registered engineers
+        </h1>
+      </div>
     </div>
-</nav>
-<div class=" w-full grid grid-cols-3 p-3 justify-center shadow list-bar" style="background: #065c8b">
-    <div class="text-center text-white font-bold flex justify-center items-center"><span class="pr-2 icon w-8 flex justify-center items-center"><img src="/Solar/dist/img/chat.png" style="display: inline"></span><h1 class="flex justify-center items-center">Get the best quotes, fast!</h1></div>
-    <div class="text-center text-white font-bold flex justify-center items-center"><span class="pr-2 icon w-8 flex justify-center items-center"><img src="/Solar/dist/img/location.png" style="display: inline"> </span><h1 class="flex justify-center items-center">Over 5,000 engineers across the UK</h1></div>
-    <div class="text-center text-white font-bold flex justify-center items-center"><span class="pr-2 icon w-8 flex justify-center items-center"><img src="/Solar/dist/img/check.png" style="display: inline"></span><h1 class="flex justify-center items-center">Gas Safe registered engineers</h1></div>
-</div>
-    <div class=" w-full grid grid-cols-3 p-3 justify-center shadow list-bar-mobile" style="background: #065c8b">
-        <div class="text-center text-white font-bold flex justify-center items-center gap-2"><span class=" icon w-8 flex justify-center items-center"><img src="/Solar/dist/img/chat.png" style="display: inline"></span><h1 class="flex justify-start items-center">Get the best quotes, fast!</h1></div>
-        <div class="text-center text-white font-bold flex justify-center items-center gap-2 "><span class=" icon w-8 flex justify-center items-center"><img src="/Solar/dist/img/location.png" style="display: inline"> </span><h1 class="flex justify-start items-center" >Over 5,000 engineers across the US</h1></div>
-        <div class="text-center text-white font-bold flex justify-center items-center gap-2" style="padding-bottom: 0px"><span class=" icon w-8 flex justify-center items-center"><img src="/Solar/dist/img/check.png" style="display: inline"></span><h1 class="flex justify-start items-center">Gas Safe registered engineers</h1></div>
+    <div
+      class="w-full grid grid-cols-3 p-3 justify-center shadow list-bar-mobile"
+      style="background: #6ab06d"
+    >
+      <div
+        class="text-center text-white font-bold flex justify-center items-center gap-2"
+      >
+        <span class="icon w-8 flex justify-center items-center"
+          ><img src="/Solar_2/dist/img/chat.png" style="display: inline"
+        /></span>
+        <h1 class="flex justify-start items-center">
+          Get the best quotes, fast!
+        </h1>
+      </div>
+      <div
+        class="text-center text-white font-bold flex justify-center items-center gap-2"
+      >
+        <span class="icon w-8 flex justify-center items-center"
+          ><img src="/Solar_2/dist/img/location.png" style="display: inline" />
+        </span>
+        <h1 class="flex justify-start items-center">
+          Over 5,000 engineers across the US
+        </h1>
+      </div>
+      <div
+        class="text-center text-white font-bold flex justify-center items-center gap-2"
+        style="padding-bottom: 0px"
+      >
+        <span class="icon w-8 flex justify-center items-center"
+          ><img src="/Solar_2/dist/img/check.png" style="display: inline"
+        /></span>
+        <h1 class="flex justify-start items-center">
+          Gas Safe registered engineers
+        </h1>
+      </div>
     </div>
 
-<div style="background-image: radial-gradient(ellipse at center,#1e5799 0,#013652 63%,#002031 100%)">
-    <div class="text-center text-white text-4xl font-bold lg:py-80 thank">Thank you for taking the time to complete our survey.</div>
-</div>
-
-</body>
+    <div style="background-color: #038b570f">
+      <div class="text-center text-[#4c4b4b] text-4xl font-bold lg:py-80 thank">
+        Thank you for taking the time to complete our survey.
+      </div>
+    </div>
+  </body>
 </html>
