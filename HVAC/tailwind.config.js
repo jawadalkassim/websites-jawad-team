79247beed6c3debc43986d199ef0bbdb02728080
Amjadvc <amjadalmrai@gmail.com>
2024-05-01 20:37:05 +0300

    edit page

--- HVAC/tailwind.config.js
+++ HVAC/tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
   content: ["./dist/**/*.{html,js,php}"],
   theme: {
     extend: {
-      colors:{
-        primaryColor:'#01314A',
-        secondaryColor:'#0d8155'
-      }
+      colors: {
+        primaryColor: "#01314A",
+        secondaryColor: "#0d8155",
+      },
+      backgroundImage: {
+        backBtnBg: "linear-gradient(45deg, #4CAF50, #2E7D32)",
+        pageBg: "linear-gradient(1deg, #5ee96380, #8dff930d)",
+      },
     },
   },
   plugins: [],
-}
-
+};
