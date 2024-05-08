504fe6fc0e82eca5c45f63a2969b6b72f1efe211
KaramDargham <karamdargham1@gmail.com>
2024-05-08 22:03:35 +0300

    change design

--- Garage/tailwind.config.js
+++ Garage/tailwind.config.js
@@ -3,12 +3,85 @@ module.exports = {
   content: ["./dist/**/*.{html,js,php}"],
   theme: {
     extend: {
-      colors:{
-        primaryColor:'#01314A',
-        secondaryColor:'#0d8155'
-      }
+      colors: {
+        primaryColor: "#01314A",
+        secondaryColor: "#0d8155",
+      },
+      backgroundImage: {
+        backBtnBg: "linear-gradient(45deg, #272727c2, #767676)",
+        pageBg: "#fff",
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
+      keyframes: {
+        "slide-next": {
+          "0%": {
+            transform: "translateX(-5%);",
+            opacity: 0.5,
+          },
+          "100%": {
+            transform: "translateX(0);",
+            opacity: 1,
+          },
+        },
+
+        "slide-prev": {
+          "0%": {
+            transform: "translateX(5%);",
+            opacity: 0.5,
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
+        },
+
+        arrowAnimateBack: {
+          " 0%": {
+            transform: "translateX(0) rotate(180deg)",
+          },
+          "25%": {
+            transform: "translateX(4px) rotate(180deg) ",
+          },
+          "50%": {
+            transform: "translateX(0px) rotate(180deg) ",
+          },
+          "75%": {
+            transform: "translateX(-4px) rotate(180deg) ",
+          },
+          "100%": {
+            transform: " translateX(0) rotate(180deg)",
+          },
+        },
+
+        arrowAnimateNext: {
+          " 0%": {
+            transform: "translateX(0) ",
+          },
+          "25%": {
+            transform: "translateX(4px)  ",
+          },
+          "50%": {
+            transform: "translateX(0px)  ",
+          },
+          "75%": {
+            transform: "translateX(-4px)  ",
+          },
+          "100%": {
+            transform: " translateX(0) ",
+          },
+        },
+      },
+
+      animation: {
+        "slide-next": "slide-next 300ms  linear ",
+        "slide-prev": "slide-prev 300ms  linear ",
+        arrowAnimateBack: "arrowAnimateBack 1000ms linear infinite ",
+        arrowAnimateNext: "arrowAnimateNext 1000ms linear infinite ",
+      },
     },
   },
-  plugins: [],
-}
 
+  plugins: [],
+};
