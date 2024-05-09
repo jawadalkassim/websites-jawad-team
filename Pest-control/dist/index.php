5f04ce465600767065453ffcb43e270f77a35709
KaramDargham <karamdargham1@gmail.com>
2024-05-09 22:28:19 +0300

    change navbar and shadow

--- Pest-control/dist/index.php
+++ Pest-control/dist/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Pest-control</title>
+    <title>Pest control</title>
     <link href="/Pest-control/dist/output.css" rel="stylesheet" />
     <style>
         .duration-btn {
