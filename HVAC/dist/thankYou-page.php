7a4edca9288aa0d9b43492c5314712e72ba981d8
KaramDargham <karamdargham1@gmail.com>
2024-05-13 23:19:55 +0300

    change style

---
+++ HVAC/dist/thankYou-page.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>thank you</title>
+  <link rel="stylesheet" href="/HVAC/dist/output.css" />
+  <link rel="icon" type="image/svg" href="/HVAC/dist/img/logo.svg">
+</head>
+<body style="background-color: #fff">
+<nav class="text-center" style="background: white">
+  <div
+          class="grid lg:grid-cols-2 md:grid-cols-1 items-center 2xl:w-contanerxl 2xl:ml-auto 2xl:mr-auto"
+  >
+    <div class="flex justify-center items-center p-2">
+      <img src="/HVAC/dist/img/logo.svg" class="w-12 brand" />
+    </div>
+    <a
+            href=""
+            class="nav-link pt-3 text-[#275329] decoration-primaryColor font-bold text-xl block lg:text-end sm:text-center md:text-center lg:mx-40 md:mx-0 sm:mx-0 pb-3 lg:w-1/2 sm:w-full"
+    >Get Free Quotes</a
+    >
+  </div>
+</nav>
+<div class="w-full shadow list-bar bg-secondaryColor">
+  <div
+          class="grid grid-cols-3 p-3 justify-center xl:w-contanerxl ml-auto mr-auto"
+  >
+    <div
+            class="text-center text-white font-bold flex justify-center items-center"
+    >
+          <span class="pr-2 icon w-8 flex justify-center items-center"
+          ><img src="/HVAC/dist/img/chat.png" style="display: inline"
+          /></span>
+      <h1 class="flex justify-center items-center">
+        Get the best quotes, fast!
+      </h1>
+    </div>
+    <div
+            class="text-center text-white font-bold flex justify-center items-center"
+    >
+          <span class="pr-2 icon w-8 flex justify-center items-center"
+          ><img src="/HVAC/dist/img/location.png" style="display: inline" />
+          </span>
+      <h1 class="flex justify-center items-center">
+        Over 5,000 engineers across the UK
+      </h1>
+    </div>
+    <div
+            class="text-center text-white font-bold flex justify-center items-center"
+    >
+          <span class="pr-2 icon w-8 flex justify-center items-center"
+          ><img src="/HVAC/dist/img/check.png" style="display: inline"
+          /></span>
+      <h1 class="flex justify-center items-center">
+        Gas Safe registered engineers
+      </h1>
+    </div>
+  </div>
+</div>
+<div
+        class="w-full grid grid-cols-3 p-3 justify-center shadow list-bar-mobile bg-secondaryColor"
+>
+  <div
+          class="text-center text-white font-bold flex justify-center items-center gap-2"
+  >
+        <span class="icon w-8 flex justify-center items-center"
+        ><img src="/HVAC/dist/img/chat.png" style="display: inline"
+        /></span>
+    <h1 class="flex justify-start items-center">
+      Get the best quotes, fast!
+    </h1>
+  </div>
+  <div
+          class="text-center text-white font-bold flex justify-center items-center gap-2"
+  >
+        <span class="icon w-8 flex justify-center items-center"
+        ><img src="/HVAC/dist/img/location.png" style="display: inline" />
+        </span>
+    <h1 class="flex justify-start items-center">
+      Over 5,000 engineers across the US
+    </h1>
+  </div>
+  <div
+          class="text-center text-white font-bold flex justify-center items-center gap-2"
+          style="padding-bottom: 0px"
+  >
+        <span class="icon w-8 flex justify-center items-center"
+        ><img src="/HVAC/dist/img/check.png" style="display: inline"
+        /></span>
+    <h1 class="flex justify-start items-center">
+      Gas Safe registered engineers
+    </h1>
+  </div>
+</div>
+
+<div>
+  <div
+          class="text-center text-gray-600 text-5xl max-[480px]:text-[43px] font-bold lg:py-44 thank leading-[60px]"
+  >
+    Thank you for taking the time to complete our survey.
+  </div>
+</div>
+</body>
+</html>
