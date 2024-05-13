04b90420295434e11c60efbb4b25cd8a0c4b6fd9
KaramDargham <karamdargham1@gmail.com>
2024-05-13 23:09:16 +0300

    change style

--- Painter/dist/index.php
+++ Painter/dist/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>Painter</title>
+      <link rel="icon" type="image/svg" href="/Painter/dist/img/logo.svg">
     <link href="/Painter/dist/output.css" rel="stylesheet" />
     <style>
       .duration-btn {
@@ -29,7 +30,7 @@
         >
       </div>
     </nav>
-    <div class="w-full shadow list-bar" style="background: #379c3bcc">
+    <div class="w-full shadow list-bar bg-secondaryColor">
       <div
         class="grid grid-cols-3 p-3 justify-center xl:w-contanerxl ml-auto mr-auto"
       >
@@ -69,8 +70,7 @@
       class="bg-white min-[1500px]:min-h-screen flex flex-col justify-center"
     >
       <div
-        class="w-full grid grid-cols-3 p-3 justify-center shadow list-bar-mobile"
-        style="background: #379c3bcc"
+        class="w-full grid grid-cols-3 p-3 justify-center shadow list-bar-mobile bg-secondaryColor"
       >
         <div
           class="text-center text-white font-bold flex justify-center items-center gap-2"
@@ -106,7 +106,7 @@
       </div>
       <!-- delete style="background-color: #038b570f  min-[992px]:pt-[100px]" -->
       <h3
-        class="min-[992px]:pt-[30px] text-center text-[55px] max-[360px]:text-[25px] max-[480px]:text-[30px] font-bold text-secondaryColor pt-3 px-3 max-[480px]:pt-4 w-full lg"
+        class="pt-[50px] text-center text-[55px] max-[360px]:text-[25px] max-[480px]:text-[30px] font-bold text-secondaryColor px-3 max-[480px]:pt-[30px] w-full lg"
       >
         Get your painter deal
       </h3>
@@ -130,7 +130,7 @@
             <form
               class="container bg-white lg:p-6 md:p-0 sm:p-0 lg:w-8/12 rounded shadow-3xl pt-2"
               id="regForm"
-              action="/Painter/dist/submit-page.php"
+              action="/Painter/dist/thankYou-page.php"
             >
                     <div class="tab text-white text-3xl font-bodyFont">
                         <div class="text- text-3xl font-semibold pt-5 text-center p-5">
@@ -407,7 +407,7 @@
     </div>
 </div>
 
-<footer class="py-[80px] px-[40px]  bg-[#4CAF50]">
+    <footer class="py-[80px] px-[40px] bg-secondaryColor">
     <div
             class="container xl:w-contanerxl pl-4 pr-4 mx-auto flex justify-center"
     >
