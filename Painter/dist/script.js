ddb53f6ef73c12997bfa1fac5e05abd03407ba92
KaramDargham <karamdargham1@gmail.com>
2024-05-17 19:26:04 +0300

    add js and inputs

--- Painter/dist/script.js
+++ Painter/dist/script.js
@@ -73,7 +73,7 @@ function back(){
 var isPhoneValid = false;
 function validateForm() {
     var x, y, i, valid = true;
-    var phoneno = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    var phoneno = /^\(?([0-9]{3})\)?[-.\s]?([0-9]{3})[-.\s]?([0-9]{4})$/;
     var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     x = document.getElementsByClassName("tab");
     y = x[currentTab].getElementsByTagName("input");
