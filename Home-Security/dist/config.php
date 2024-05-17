35da07a0a727cbe69c55fcd9840ea774a55b2dd2
KaramDargham <karamdargham1@gmail.com>
2024-05-17 20:45:01 +0300

    clear config data

--- Home-Security/dist/config.php
+++ Home-Security/dist/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $campaign_data = array(
-	'lp_campaign_id' => '65fc54578ff8f'
-	, 'lp_campaign_key' => 'Vtp4PH8MNyndDmL3gBZR'
+	'lp_campaign_id' => ''
+	, 'lp_campaign_key' => ''
 	, 'lp_test' => ''// 1 for testing
 );
 
